01ca90f9577a26e6665a0169205393d135118b14
softengineer123 <engineerwork@protonmail.com>
2020-04-18 05:46:48 +0530

    Update urls.py

--- Django REST Framework Backend/messenger/chat/urls.py
+++ Django REST Framework Backend/messenger/chat/urls.py
@@ -1,14 +1,17 @@
 from django.urls import include, path
 from rest_framework import routers
 from . import views
-from rest_framework.authtoken.views import ObtainAuthToken
+from .views import CustomAuthToken
+from rest_framework.authtoken import views as viewsrest
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'chats', views.ChatViewViewSet)
 
 urlpatterns = [
+    path('getcurrentuser/', views.get_current_user_id, name='get_crnt_user'),
+    path('signup/', views.signup, name='new_signup'),
     path('createchat/', views.create_new_chat, name='create_chat'),
     path('', include(router.urls)),
-    path('api-auth/', ObtainAuthToken.as_view()),
+    path('api-auth/', CustomAuthToken.as_view()),
 ]
