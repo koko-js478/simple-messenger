0cab2a08d66fb9de33a7adae0628e89fea636c64
softengineer123 <engineerwork@protonmail.com>
2020-04-18 05:46:27 +0530

    Update user.service.ts

--- Angular Frontend Server/messenger-frontend/src/app/user.service.ts
+++ Angular Frontend Server/messenger-frontend/src/app/user.service.ts
@@ -15,7 +15,7 @@ export class UserService {
         ) { }
 
     registerUser(userData): Observable<any> {
-        return this.http.post('http://127.0.0.1:8000/chat/users/', userData);
+        return this.http.post('http://127.0.0.1:8000/chat/signup/', userData);
     }    
 
     loginUser(userData): Observable<any> {
@@ -24,5 +24,17 @@ export class UserService {
 
     getallUsers(): Observable<any> {
         return this.http.get('http://127.0.0.1:8000/chat/users/?format=json');
+    }
+
+    
+
+    getCurrentUser(tokenkey): Observable<any> {
+        const httpOptions = {
+            headers: new HttpHeaders({
+            'Content-Type':  'application/json',
+            'Authorization': `Token ${tokenkey}`
+            })
+        };
+        return this.http.get('http://127.0.0.1:8000/chat/getcurrentuser/', httpOptions);
     }
 }
