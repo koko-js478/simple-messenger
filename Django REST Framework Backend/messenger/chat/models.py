e6ee01a1b31abf2b0af1af5d3214ffdb4622a1e1
softengineer123 <engineerwork@protonmail.com>
2020-04-18 05:46:45 +0530

    Update models.py

--- Django REST Framework Backend/messenger/chat/models.py
+++ Django REST Framework Backend/messenger/chat/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 class ChatView(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, default=None, related_name='sender')
@@ -13,7 +17,12 @@ class ChatView(models.Model):
         return str(self.id)
 
     def create_chat(message, senderid, receiverid):
-        senderid = int(senderid)
-        receiverid = int(receiverid)
+        senderid = int(senderid) + 1
+        receiverid = int(receiverid) + 1
         chatnew = ChatView(sender = get_object_or_404(User, id=senderid), receiver = get_object_or_404(User, id=receiverid), sent_date = timezone.now(), textcontent = message)
         chatnew.save()
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
